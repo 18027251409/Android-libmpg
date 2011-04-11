ef6d7e8f921490254123310fb6f5118d5066124e
Dale King <dalewking@gmail.com>
2011-04-11 13:01:45 -0400

    Changes to get unit testing working

---
+++ Android-libmpg_Test/src/nobleworks/libmpg/test/LibmpgTest.java
@@ -0,0 +1,11 @@
+package nobleworks.libmpg.test;
+
+import android.test.AndroidTestCase;
+
+public class LibmpgTest extends AndroidTestCase
+{
+    public void testPlaceholder()
+    {
+        assertNull(null);
+    }
+}
